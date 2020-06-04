45fc70df8ff29b293c1df8665f843a54abcff09a
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2020-06-03 22:25:05 -0400

    Finished lect7: Comparing JSX and React.createElement

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
+import React from "react";
+import ReactDOM from "react-dom";
 
-// const container = document.getElementById('app');
+// const jsx = <h1>Hello, Platzi Badges!!</h1>;
+// const element = React.createElement(
+//   "a",
+//   { href: "https://platzi.com" },
+//   "Ir a Platzi!"
+// );
 
-// constainer.appendChild(element);
+// const name = "Jorge";
+// // const element = React.createElement("h1", {}, `Hola soy ${name}`);
+// const jsx = <h1>Hola soy, {name}</h1>;
+const container = document.getElementById("app");
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+const jsx = (
+  <div>
+    <h1>Soy Jorge</h1>
+    <p>Soy ingeniero frontend.</p>
+  </div>
+);
 
-const element = <h1>Hello, Platzi Badges!!</h1>
+const element = React.createElement(
+  "div",
+  {},
+  React.createElement("h1", {}, "Hola, soy Jorge"),
+  React.createElement("p", {}, "Hola, soy Jorge")
+);
 
-const container = document.getElementById('app');
-
-ReactDOM.render(__que__, __donde__);
+// ReactDOM.render(__que__, __donde__);
 ReactDOM.render(element, container);
