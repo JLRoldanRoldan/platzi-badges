3543c5ba3567397b3983672e2ab4a0d9c59409c6
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2020-06-03 21:44:38 -0400

    Finished lect6: Introduction to React and ReactDOM

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
+// const element = document.createElement('h1');
+// element.innerText = 'Hello, Platzi Badges!';
+
+// const container = document.getElementById('app');
+
+// constainer.appendChild(element);
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const element = <h1>Hello, Platzi Badges!!</h1>
+
 const container = document.getElementById('app');
 
-const element = <h1>Hello, Platzi Badges</h1>;
+ReactDOM.render(__que__, __donde__);
 ReactDOM.render(element, container);
