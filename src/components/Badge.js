8d86aae9ce3def3b4865edf571e52df3dba72691
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2020-06-05 22:14:58 -0400

    Finished lect11

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,30 +1,32 @@
 import React from "react";
 import { render } from "react-dom";
 
+import "./styles/Badge.css";
 import confLogo from "../images/badge-header.svg";
+import avatar from "../images/image.png";
 
 class Badge extends React.Component {
   render() {
     return (
-      <div>
-        <div>
+      <div className="Badge">
+        <div className="Badge__header">
           <img src={confLogo} alt="Logo de la conferencia" />
         </div>
 
-        <div>
-          <img src="" alt="Avatar" />
+        <div className="Badge__section-name">
+          <img className="Badge__avatar" src={avatar} alt="Avatar" />
           <h1>
             Jorge <br />
             Roldan-Roldan
           </h1>
         </div>
 
-        <div>
-          <p>Frontend Engineer</p>
-          <p>@JLRoldanRoldan</p>
+        <div className="Badge__section-info">
+          <h3>Frontend Engineer</h3>
+          <div>@JLRoldanRoldan</div>
         </div>
 
-        <div>#platziconf</div>
+        <div className="Badge__footer">#platziconf</div>
       </div>
     );
   }
