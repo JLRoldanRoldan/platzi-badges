6013219f865f8982758b012cb2a0864e7f543c08
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2020-06-12 13:38:09 -0400

    Finished lect13

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,18 @@
 import React from "react";
-import logo from "../images/logo.svg"
+import logo from "../images/logo.svg";
+import "./styles/Navbar.css";
 
 class Navbar extends React.Component {
   render() {
     return (
-      <div>
-        <a href={logo} alt="Logo">
-					<img src={logo} alt="Logo" />
-					<span className="font-weight-light" >Platzi</span>
-					<span className="font-weight-light" >Conf</span>
-				</a>
+      <div className="Navbar">
+        <div className="container-fluid">
+          <a className="Navbar__brand" href="/" alt="Logo">
+            <img className="Navbar__brand-logo" src={logo} alt="Logo" />
+            <span className="font-weight-light">Platzi</span>
+            <span className="font-weight-bold">Conf</span>
+          </a>
+        </div>
       </div>
     );
   }
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,12 +1,33 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import header from "../images/badge-header.svg";
+import "../components/styles/BadgeNew.css";
+import Badge from "../components/Badge";
+import avatar from "../images/image.png";
 
 class BadgeNew extends React.Component {
   render() {
     return (
-			<div>
-				<Navbar />
-			</div>
+      <div>
+        <Navbar />
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={header} alt="Logo" />
+        </div>
+				
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <Badge
+                firstName="Jorge"
+                lastName="Roldan-Roldan"
+                twitter="JLRoldanRoldan"
+                jobTitle="Frontend Engineer"
+                avatar={avatar}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
