620032e845a8b0fc42430d6c2b8f2538ce6888ea
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2020-06-13 12:46:41 -0400

    Lect15: Completed the form 5:51 min

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -2,7 +2,11 @@ import React from "react";
 
 class BadgeForm extends React.Component {
   handleChange = (e) => {
-    console.log({ name: e.target.name, value: e.target.value });
+    // console.log({ name: e.target.name, value: e.target.value });
+
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
   };
 
   handleClick = (e) => {
@@ -29,6 +33,45 @@ class BadgeForm extends React.Component {
             />
           </div>
 
+          <div className="form-group">
+            <label htmlFor="">Last Name</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              name="lastName"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="">Email</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="email"
+              name="email"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="">Job Title</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              name="jobTitle"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="">Twitter</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              name="Twitter"
+            />
+          </div>
           <button onClick={this.handleClick} className="btn btn-primary">
             Save
           </button>
