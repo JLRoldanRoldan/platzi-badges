3aaf26f532ba886b0c138ede02f8448030d874e5
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2020-06-13 12:23:30 -0400

    Lect14: Used type=button for button (7:16 min)

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,8 +1,39 @@
 import React from "react";
 
 class BadgeForm extends React.Component {
+  handleChange = (e) => {
+    console.log({ name: e.target.name, value: e.target.value });
+  };
+
+  handleClick = (e) => {
+    console.log("Button was clicked");
+  };
+
   render() {
-    return <p>BadgeForm</p>;
+    return (
+      <div>
+        <h1>New Attendant</h1>
+        <form>
+          <div className="form-group">
+            <label htmlFor="">First Name</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              name="firstName"
+            />
+          </div>
+
+          <button
+            type="button"
+            onClick={this.handleClick}
+            className="btn btn-primary"
+          >
+            Save
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
